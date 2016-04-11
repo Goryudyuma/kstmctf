88f2ddf0b6fa23cb44ab3e40450a71a1a0ff92ea
Kitamura Yuta <ugwis@icloud.com>
2016-04-12 03:32:22 +0900

    フォームの文字をラベル化

--- kstmctf/resources/views/create.blade.php
+++ kstmctf/resources/views/create.blade.php
@@ -6,15 +6,15 @@
 <!--	<label class="control-label">Flagを入力</label>-->
 {{Form::open(['action'=>'MainController@createcheck'])}}
 <div class="form-group">
-title
+{{Form::label('title','Title')}}
 {{Form::text('title',null,['class'=>'form-control'])}}
 </div>
 <div class="form-group">
-url 
+{{Form::label('url','URL')}} 
 {{Form::input('url','url',null,['class'=>'form-control'])}}<br/>
 </div>
 <div class="form-group">
-Flag
+{{Form::label('Flag')}}
 {{Form::text('flag',null,['class'=>'form-control'])}}
 </div>
 {{Form::submit()}}
