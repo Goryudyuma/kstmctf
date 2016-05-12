40adb32bf30dbb24a9bc90f9aec9b5a8314d0873
root <root@kstm-ctf.kstm.shinshu-u.ac.jp>
2016-05-13 01:04:51 +0900

    コンテンツページをいい感じに

--- kstmctf/resources/views/content.blade.php
+++ kstmctf/resources/views/content.blade.php
@@ -1,15 +1,79 @@
 @extends('master')
 
 @section('content')
-問題番号:{{$question['id']}}<br />
+<h1>問題{{$question['id']}}:{{$question['title']}}</h1>
 問題文を見た人数:{{$question['challengecount']}}<br />
 解けた人数:{{$question['solvedcount']}}<br />
 @if ($question['solvedcount'] !== 0)
 平均時間:{{$question['avetime']}}秒<br />
-@foreach ($question['mintime'] as $var)
+@endif
+</div>
+<div class="container" style="margin-top:20px;">
+{{Html::link('/question/'.$question['id'], "問題を開く", ['about'=>'_blank','class'=>'btn btn-primary btn-lg btn-block'])}}
+@if ($question['solvedcount'] !== 0)
+</div>
+<div class="container" style="margin-top:20px;">
+<div id="graph"></div>
+</div>
+<script>
+var scale = {
+	1:"1秒~",
+	30:"30秒~",
+	60:"1分~",
+	1800:"30分~",
+	3600:"1時間~",
+	43200:"12時間~",
+	86400:"24時間~",
+	1296000:"15日~",
+	2592000:"1ヵ月~",
+	31536000:"1年~",
+	315360000:"10年~"
+};
+var chart;
+$.getJSON("../contentjson/{{$question['id']}}",function(data){
+	var values = [0,0,0,0,0,0,0,0,0,0,0];
+	var categories = [];
+	for(key in scale){
+		categories.push(scale[key]);
+	}
+	for(var i=0;i<data.length;i++){
+		for(key in scale){
+			if(data[i] >= scale[key]){
+				values[key]++;
+				break;
+			}
+		}
+	}
+	console.log(values);
+	var options = {
+		title:{
+			text:null
+		},
+		chart:{
+			renderTo:"graph",
+			type:"column"
+		},
+		yAxis:{
+			title:{
+				text:"正答数"
+			},
+			allowDecimals:false
+		},
+		xAxis:{
+			title:{
+				text:"解くのにかかった時間"
+			},
+			categories:categories
+		},
+		series:[{name:"solved time",data:values}]
+	}
+	chart = new Highcharts.Chart(options);
+});
+</script>
+<!--@foreach ($question['mintime'] as $var)
 {{$var['nickname']}}
 {{$var['time']}}<br />
-@endforeach
+@endforeach-->
 @endif
-リンク:{{Html::link('/question/'.$question['id'], $question['title'], 'target="_blank"')}}
 @stop
+
